d5e059d8a016a53bddba6b7c7b9362b57c43ade9
shaunphommachack <113938319+shaunphommachack@users.noreply.github.com>
2022-10-03 22:20:38 -0700

    timed quiz

---
+++ assets/questions.js
@@ -0,0 +1,22 @@
+var questions = [
+    {
+        title: "what doc type is used in an index file",
+        choices: ["html", "css", "js", "png"],
+        answer: "html"
+    },
+    {
+        title: "what is 2+2",
+        choices: ["3", "6", "4", "22"],
+        answer: "4"
+    },
+    {
+        title: "which one of these are the hardest",
+        choices: ["css", "java", "learning to drive a car", "html"],
+        answer: "java"
+    },
+    {
+        title: "what is this quiz suppose to be about",
+        choices: ["code", "math", "science", "english"],
+        answer: "code"
+    },
+]
